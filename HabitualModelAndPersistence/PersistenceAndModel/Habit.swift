4542d1f2ef0f435c5c08811f5b18579ba250ccee
noah <noahwoodward2244@gmail.com>
2018-11-19 17:27:48 -0500

    Finished Habit Model

--- HabitualModelAndPersistence/PersistenceAndModel/Habit.swift
+++ HabitualModelAndPersistence/PersistenceAndModel/Habit.swift
@@ -9,17 +9,49 @@
 import Foundation
 import UIKit
 
-struct Habit {
+struct Habit: Codable {
     var title: String
-    let dateCreated: Date
-    let selectedImage: UIImage
-    var currentStreak: Int
-    var bestStreak: Int
+    let dateCreated: Date = Date()
+    let selectedImage: Habit.Images
+    var currentStreak: Int = 0
+    var bestStreak: Int = 0
     var lastCompletionDate: Date?
-    var numberOfCompletions: Int
+    var numberOfCompletions: Int = 0
     
     var hasCompletedForToday: Bool{
         return lastCompletionDate?.isToday ?? false
+    }
+    
+    enum Images: Int, CaseIterable, Codable {
+        case book
+        case bulb
+        case clock
+        case code
+        case drop
+        case food
+        case grow
+        case gym
+        case heart
+        case lotus
+        case other
+        case pet
+        case pill
+        case search
+        case sleep
+        case tooth
+        
+        var image: UIImage {
+            guard let image = UIImage(named: String(describing: self)) else {
+                fatalError("image \(self) not found")
+            }
+            
+            return image
+        }
+    }
+    
+    init(title: String,image: Habit.Images) {
+        self.title = title
+        self.selectedImage = image
     }
     
 }
