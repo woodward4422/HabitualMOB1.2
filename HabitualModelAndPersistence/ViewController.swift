3ba02a58fdbb1c41e9f3f71eac198cf197f0d4e8
noah <noahwoodward2244@gmail.com>
2018-11-19 16:55:45 -0500

    Added starter project

---
+++ HabitualModelAndPersistence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HabitualModelAndPersistence
+//
+//  Created by Noah Woodward on 11/19/18.
+//  Copyright © 2018 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
